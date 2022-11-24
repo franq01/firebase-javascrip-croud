b46202879ff890530d7b77951497cb715796ee52
franq01 <carreone705@gmail.com>
2022-11-24 12:00:02 -0600

    minuto 22:38

--- index.js
+++ index.js
@@ -1,18 +1,12 @@
 
-  // Import the functions you need from the SDKs you need
-  import { initializeApp } from "https://www.gstatic.com/firebasejs/9.14.0/firebase-app.js";
-  // TODO: Add SDKs for Firebase products that you want to use
-  // https://firebase.google.com/docs/web/setup#available-libraries
+window.addEventListener('DOMContentLoaded', ()=>{
 
-  // Your web app's Firebase configuration
-  const firebaseConfig = {
-    apiKey: "AIzaSyD-6PXBK9EPAfVjw9uu06XeRtanCsmnnNE",
-    authDomain: "fir-javascrip-croud.firebaseapp.com",
-    projectId: "fir-javascrip-croud",
-    storageBucket: "fir-javascrip-croud.appspot.com",
-    messagingSenderId: "16966987579",
-    appId: "1:16966987579:web:21993da401616c8d8e511e"
-  };
+})
 
-  // Initialize Firebase
-  const app = initializeApp(firebaseConfig);
+document.getElementById('task-form')
+
+taskForm.addEventListener('submit', (e) => {
+    e.preventDefault()
+    const title = taskForm['task-title']
+    const description = taskForm['task-description']
+})
